c9c79c4cfb6373d5a7c22f3310bcc817b9339658
LeonardoAlmeidaRibas <leo.almeidaribas21@gmail.com>
2024-10-29 19:01:29 -0300

    Feito

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import "dotenv/config";
 
 const connectDB = async() => {
     try {
         await mongoose.connect(
-            "mongodb+srv://LeonardoAlmeida:leoribas2007@cluster0.n65t3.mongodb.net/compras"
+          `mongodb+srv://LeonardoAlmeida:leoribas2007@cluster0.n65t3.mongodb.net/compras`
             // `mongoose.connect('mongodb+srv://<DB_USER>:<PASSWORD>i@<CLUSTER_NAME>/<COLLECTION_NAME>');`
         );
 
